3133b5d3601e49bf40037d425cee4df83497a265
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2024-05-07 20:52:26 -0400

    Update janko.at-show-puzzle-data.js

--- janko.at-show-puzzle-data.js
+++ janko.at-show-puzzle-data.js
@@ -2,7 +2,7 @@
 // @name         janko.at-show-puzzle-data
 // @namespace    http://tampermonkey.net/
 // @version      2024-05-08
-// @description  try to take over the world!
+// @description  show the parseable data for the puzzle
 // @author       You
 // @match        https://www.janko.at/Raetsel/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
@@ -12,10 +12,15 @@
 window.addEventListener('load', () => {
     let data = document.getElementById('data');
     if (data !== undefined) {
+        let copy = document.createElement('button');
+        copy.onclick = () => navigator.clipboard.writeText(data.innerText);
+        copy.innerHTML = 'copy data';
+        document.body.appendChild(copy);
         let text = document.createElement('verbatim');
         text.innerText = data.innerText;
         let textcontainer = document.createElement('details');
         let textsummary = document.createElement('summary');
+        textsummary.style = 'background: white;';
         textsummary.innerHTML = 'show puzzle data';
         textcontainer.appendChild(textsummary);
         let textdiv = document.createElement('div');
