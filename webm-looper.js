f0c15912f4aff0dfc2d5b3b74224f8092485e2f1
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2024-05-07 20:14:24 -0400

    Create webm-looper.js

---
+++ webm-looper.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Webm Looper
+// @namespace   com.whatisthisimnotgoodwithcomputers.webmlooper
+// @author      WhatIsThisImNotGoodWithComputers
+// @description A userscript which automatically enables looping on Webm videos in your browser.
+// @include     *.webm
+// @include     *.mp4
+// @include     *.mp3
+// @run-at      document-start
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+var vids = document.getElementsByTagName("video");
+for (i = 0; i < vids.length; i++) vids[i].setAttribute("loop", "true");
+void 0;
